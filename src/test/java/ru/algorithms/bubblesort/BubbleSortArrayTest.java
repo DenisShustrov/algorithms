c1c071d44cf660701766d641d3dbae3946775c12
DenisShustrov <denisshustroff@yandex.ru>
2019-05-18 14:34:33 +0300

    My bubble sort array

--- src/test/java/ru/algorithms/bubblesort/BubbleSortArrayTest.java
+++ src/test/java/ru/algorithms/bubblesort/BubbleSortArrayTest.java
@@ -1,20 +1,24 @@
 package ru.algorithms.bubblesort;
 
-import org.junit.Assert;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 /**
  * Test.
+ *
  * @author dshustrov
  * @version 1
  * @since 17.05.2019
  */
 public class BubbleSortArrayTest {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     private int[] array;
 
@@ -22,18 +26,14 @@ public class BubbleSortArrayTest {
     public void whenArrayNotSortThenArraySort() throws MyException {
         int[] array = {5, 1, 2, 7, 3, 0, -87, -1};
         int[] resultArray = BubbleSortArray.sort(array);
-        int[] expectArray =  {-87, -1, 0, 1, 2, 3, 5, 7};
+        int[] expectArray = {-87, -1, 0, 1, 2, 3, 5, 7};
         assertThat(resultArray, is(expectArray));
     }
 
     @Test
-    public void whenSortArrayTheyThrowMyException() {
-
-        try {
-            BubbleSortArray.sort(array);
-            Assert.fail();
-        } catch (MyException me) {
-             Assert.assertEquals("Array must be initialized!", me.getMessage());
-        }
+    public void whenSortArrayTheyThrowMyException() throws MyException {
+        thrown.expect(MyException.class);
+        thrown.expectMessage(equalTo("Array must be initialized!"));
+        BubbleSortArray.sort(array);
     }
 }
